9bb9f4c1319facaaf4824a2b03f0c997b1f70ac0
Lucas <lucas_percisi@hotmail.com>
2018-06-10 22:38:37 -0300

    Geração do AFND dos tokens

--- input-file.py
+++ input-file.py
@@ -4,6 +4,7 @@ f = open('tokens_GRs.txt','r')
 for line in f:
     tokens.append(line)
 f.close()
+print (tokens,'TOKENS')
 
 #remove os '\n' dos tokens
 for i, token in enumerate(tokens):
@@ -28,4 +29,49 @@ print (tokens,'TOKENS')
 print (chars_tokens,'UNIAO DOS CARACTERS DOS TOKENS\n')
 print (first_chars_tokens,'CARACTERES INICIAIS\n')
 
-#return [token, char_tokens, first_char_tokens]
+
+afnd = []
+
+def addedChar_afnd(char):
+
+    for state in range(0,len(afnd)):
+        if afnd[state][2] == char:
+            return True
+    return False
+
+def bucaPosChar_afnd(char):
+
+    for state in range(0,len(afnd)):
+        if afnd[state][2] == char:
+            return state
+    return False
+
+state = 0
+for token in range(0, len(tokens)):
+    for char in range(0, len(tokens[token])):
+        
+        if char == 0: #ESTADO INICIAL
+
+            
+            if not addedChar_afnd(tokens[token][char]):#NÃO FOI ADICIONADO
+                afnd.append([(True,state),[state+1],tokens[token][char]])
+                state = state + 1
+            else:#JÁ FOI ADICIONADO
+                pos = bucaPosChar_afnd(tokens[token][char])
+                afnd[pos][1].append(state+1)
+                if not afnd[pos][0][0]: afnd[pos][0] = (True,afnd[pos][0][1])
+                state = state + 1
+
+        else: #ESTADO NÃO FINAL
+            afnd.append([(None,state),[state+1],tokens[token][char]])
+            state = state + 1
+
+        
+        #ADICIONA ESTADO FINAL
+        if char == len(tokens[token])-1:
+            afnd.append([(False,state),[None],None])
+            state = state + 1
+                
+print("True = Estado Inicial; False = Estado Final; None = Estado não final nem inicial\n")
+for state in range(0,len(afnd)):
+    print(afnd[state])
