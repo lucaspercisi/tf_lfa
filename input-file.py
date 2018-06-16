2ee359b425c3f1710a2e1d7d077a878e4c918de3
Lucas <lucas_percisi@hotmail.com>
2018-06-16 18:20:27 -0300

    Estrtura modificado para dicionario

--- input-file.py
+++ input-file.py
@@ -1,28 +1,10 @@
-#retorna true se o parametro ja foi adicionado no afnd
-def addedChar_afnd(char):
-
-    for state in range(0,len(afnd)):
-        if afnd[state][2] == char:
-            return True
-    return False
-
-
-#busca a posição do parametro dentro do afnd
-def bucaPosChar_afnd(char):
-
-    for state in range(0,len(afnd)):
-        if afnd[state][2] == char:
-            if afnd[state][0][0]:
-                return state
-    return -1
-
-
 #adiciona os tokens do arquivo em uma lista
 tokens = []
 f = open('tokens_GRs.txt','r')
 for line in f:
     tokens.append(line)
 f.close()
+print (tokens,'TOKENS')
 
 #remove os '\n' dos tokens
 for i, token in enumerate(tokens):
@@ -30,46 +12,81 @@ for i, token in enumerate(tokens):
     if pos > -1:
         token = token[:pos]
     tokens[i] = token
-print ('TOKENS: ',tokens)
+
+#cria uma lista com a uniao dos caracteres dos tokens
+chars_tokens = []
+for i in range(0, len(tokens)):
+    for j in range(0, len(tokens[i])):
+        if tokens[i][j] not in chars_tokens:
+            chars_tokens.append(tokens[i][j])
+
+#cria lista contendo os caracteres iniciais de cada token
+first_chars_tokens = []
+for i in range(0, len(tokens)):
+    first_chars_tokens.append(tokens[i][0])
+
+print (tokens,'TOKENS')
+print (chars_tokens,'ALFABETO\n')
+print (first_chars_tokens,'SIMBOLOS INICIAIS\n')
 
 
-#carrega o afnd
-afnd = []
+afnd = dict()
+
+def addedChar_afnd(char):
+    
+    for i in range(0,len(afnd)):
+        if char in afnd[i]:
+            return True
+
+    return False
+
+
+def bucaPosChar_afnd(char):
+
+    for state in range(0,len(afnd)):
+        if char in afnd[state]:
+            if afnd[state][char][1]:
+                return state
+    return -1
+
+
 state = 0
 for token in range(0, len(tokens)):
     for char in range(0, len(tokens[token])):
+
+        symbol = tokens[token][char]
         
         if char == 0: #ESTADO INICIAL TOKEN
             
-            if not addedChar_afnd(tokens[token][char]):#NÃO FOI ADICIONADO
-
-                afnd.append([(True,state),[state+1],tokens[token][char]])
+            if not addedChar_afnd(symbol):#NÃO FOI ADICIONADO
+                afnd.update({state:{symbol:[[state+1], True]}}) #TRUE
                 state = state + 1
 
             else:#JÁ FOI ADICIONADO
                 
-                pos = bucaPosChar_afnd(tokens[token][char]) #ENCONTRA A POSIÇÃO NO AFND
-                
-                if afnd[pos][0][0]: #ESTADO INICIAL AFND
-                    afnd[pos][1].append(state)
+                key = bucaPosChar_afnd(symbol) #ENCONTRA A POSIÇÃO NO AFND
+
+                if key >= 0: #ESTADO INICIAL AFND
+                    afnd[key][symbol][0].append(state) #TRUE
 
                 else: #ESTADO NÃO INICIAL AFND
-                    afnd.append([(True,state),[state+1],tokens[token][char]])
+                    afnd.update({state:{symbol:[[state+1], True]}})
                     state = state + 1
-                
 
-        else: #ESTADO NÃO INICIAL DO TOKEN
-            
-            afnd.append([(None,state),[state+1],tokens[token][char]])
+        else: #ESTADO NÃO FINAL
+            afnd.update({state:{symbol:[[state+1], None]}})
             state = state + 1
 
-
-        #ADICIONA ESTADO FINAL NO AFND
+        #ADICIONA ESTADO FINAL
         if char == len(tokens[token])-1:
-            afnd.append([(False,state),[None],None])
+            #afnd.append([(False,state),[None],None])
+            afnd.update({state:{None:[[None], False]}})#FALSE
+
             state = state + 1
 
-            
-print("\nAFND\nTrue = Estado Inicial; False = Estado Final; None = Estado nem final nem inicial\n")
+print("True = Estado Inicial; False = Estado Final; None = Estado não final nem inicial\n")
 for state in range(0,len(afnd)):
-    print(afnd[state])
+    print(state,':',afnd[state])
+
+print ("\nDETERMINIZAÇÃO:\n")
+
