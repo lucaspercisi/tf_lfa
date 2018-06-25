9a445df43e4931e3ab3d664e2195462da92b11e1
Bruno Ribeiro <brunoribeiroinf@gmail.com>
2018-06-25 15:30:41 -0300

    determinização concluída, falta testar

--- af/inputfile.py
+++ af/inputfile.py
@@ -1,4 +1,7 @@
+import copy
+
 EPSILON = '&'
+
 
 def is_sublist(sublist, list):
     return set(sublist) <= set(list)
@@ -13,21 +16,33 @@ class AFNDLine(dict):
 
 class Constructor(object):
 
-    def __init__(self, afnd, filename):
-        self.afnd = afnd
+    def __init__(self, filename):
+        self.afnd = dict()
+        self.afd = dict()
         self.file = open(filename, 'r')
         self.tokens = []
         self.alphabet = []  # alfabeto da linguagem
         self.initial_state = 0  # estado inicial
         self.state = 0  # estado incremento
-
+        self.related_states = {}  # dicionário para relacionar indeterminismos à novos estados
         self.afnd_line(self.initial_state, initial=True)  # cria o estado inicial
 
-    def print(self):
+    def print_afnd(self):
         print('Tokens encontrados: {}'.format(self.tokens))
         print('Alfabeto da linguagem: {}'.format(self.alphabet))
         print('AFND:')
         for key, value in self.afnd.items():
+            i = ''
+            f = ''
+            if value.initial:
+                i = '->'
+            if value.final:
+                f = '*'
+            print('{}{}{}: {}'.format(i, f, key, value))
+
+    def print_afd(self):
+        print('AFD:')
+        for key, value in self.afd.items():
             i = ''
             f = ''
             if value.initial:
@@ -69,11 +84,36 @@ class Constructor(object):
             if sym not in self.afnd[state]:
                 self.afnd[state][sym] = []
 
+    def afd_line(self, state, initial=False, final=False, alphabet=None):
+        """
+        Cria uma nova linha no AFD com o estado e preenche as colunas com listas vazias em cada posição
+        correspondente do alfabeto
+        :param state: int: estado a ser criado
+        :param initial: bool: se o estado informado é inicial
+        :param final: bool: se o estado informado é final
+        :param alphabet: list: lista de chars
+        """
+        if alphabet:
+            self.update_alphabet(alphabet)  # atualiza o alfabeto
+
+        if state not in self.afd:  # se o estado não existe cria-se uma nova linha
+            self.afd[state] = AFNDLine(initial=initial, final=final)
+
+        # atualiza se o estado é inicial e/ou final
+        if initial:
+            self.afd[state].initial = initial
+        if final:
+            self.afd[state].final = final
+
+        for sym in self.alphabet:  # cria uma coluna para cada símbolo do alfabeto informado que ainda não existe
+            if sym not in self.afd[state]:
+                self.afd[state][sym] = []
+
     def update_afnd(self):
         """
         Preenche as lacunas vazias do AFND
         """
-        for state, content in self.afnd.items():
+        for state in list(self.afnd):
             self.afnd_line(state)
 
     def add_afnd_step(self, origin_state, dest_state, symbol, origin_initial, origin_final, dest_initial, dest_final):
@@ -92,7 +132,23 @@ class Constructor(object):
 
         self.afnd[origin_state][symbol].append(dest_state)
         self.afnd[origin_state][symbol] = list(set(self.afnd[origin_state][symbol]))
-        self.afnd[origin_state][symbol].sort()
+
+    def add_afd_step(self, origin_state, dest_state, symbol, origin_initial, origin_final, dest_initial, dest_final):
+        """
+        Adiciona um passo no autômato determinístico
+        :param origin_state: int: estado de origem
+        :param dest_state: int: estado de destino
+        :param symbol: str: símbolo reconhecido
+        :param origin_initial: bool: estado de origem é inicial?
+        :param origin_final: bool: estado de origem é final?
+        :param dest_initial: bool: estado de destino é inicial?
+        :param dest_final: bool: estado de destino é final?
+        """
+        self.afd_line(origin_state, origin_initial, origin_final, [symbol])
+        self.afd_line(dest_state, dest_initial, dest_final)
+
+        self.afd[origin_state][symbol].append(dest_state)
+        self.afd[origin_state][symbol] = list(set(self.afd[origin_state][symbol]))
 
     def productions_to_dict(self, productions=''):
         """
@@ -174,6 +230,8 @@ class Constructor(object):
                                                dest_final=(i == len(token)-1))
 
                         self.state += 1
+
+                self.state += 1
             # GR
             else:
                 lines = block.splitlines()
@@ -207,3 +265,54 @@ class Constructor(object):
                                            dest_final=False)
 
         self.update_afnd()  # atualiza preenchendo as colunas vazias
+
+        self.afd = copy.deepcopy(self.afnd)  # cria uma cópia do AFND para o AFD que vamos mexer
+        changed = self.afnd_determinization()  # determiniza o afnd
+        while changed:
+            changed = self.afnd_determinization()
+
+    def afnd_determinization(self):
+        """
+        Cria uma cópia do AFND e determiniza esta cópia
+        """
+        changed = False
+
+        for state in list(self.afd):
+            for symbol in list(self.afd[state]):
+                state_list = self.afd[state][symbol]
+
+                if len(state_list) not in (0, 1):  # existe indeterminismo
+                    changed = True
+
+                    new_state = state_list
+                    new_state.sort()
+                    new_state = str(new_state)
+
+                    dest_final = False
+
+                    # se um dos estados for final o novo também será
+                    for s in state_list:
+                        if self.afd[s].final:
+                            dest_final = True
+                            break
+
+                    if new_state not in self.related_states:
+                        self.related_states[new_state] = self.state
+                        self.state += 1
+
+                    self.add_afd_step(origin_state=state,
+                                      dest_state=self.related_states[new_state],
+                                      symbol=symbol,
+                                      origin_initial=self.afd[state].initial,
+                                      origin_final=self.afd[state].final,
+                                      dest_initial=False,
+                                      dest_final=dest_final)
+
+                    for s in state_list:
+                        for sy in list(self.afd[s]):
+                            states = self.afd[s][sy]
+                            self.afd[self.related_states[new_state]][sy] = list(set(self.afd[self.related_states[new_state]][sy]+states))
+
+                    self.afd[state][symbol] = [self.related_states[new_state]]
+
+        return changed
